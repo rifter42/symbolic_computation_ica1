8e096c2bc4a3453feda963c2dd0a5d25cbb3a3c2
Feynman43 <mohammed.znitar@praguecollege.cz>
2021-11-04 12:37:52 +0100

    importing parks paths/files

---
+++ src/enlive.clj
@@ -0,0 +1,21 @@
+;;defining namespace
+(ns enlive
+  (:require [net.cgrand.enlive-html :as html]))
+
+;;binding parks .html paths to variabes for clarity
+(def letenske_sady "./resources/parks_html/letenske_sady.html")
+(def bertramka "./resources/parks_html/bertramka.html")
+(def frantiskanska_zahrada "./resources/parks_html/frantiskanska_zahrada.html")
+(def kampa "./resources/parks_html/kampa.html")
+(def kinskeho_zahrada "./resources/parks_html/kinskeho_zahrada.html")
+(def klamovka "./resources/parks_html/klamovka.html")
+(def ladronka "./resources/parks_html/ladronka.html")
+(def obora_hvezda "./resources/parks_html/obora_hvezda.html")
+(def petrin "./resources/parks_html/petrin.html")
+(def riegrovy_sady "./resources/parks_html/riegrovy_sady.html")
+(def stromovka "./resources/parks_html/stromovka.html")
+(def vojanovy_sady "./resources/parks_html/vojanovy_sady.html")
+(def vysehrad "./resources/parks_html/vysehrad.html")
+
+
+
