bfb5e4cf5e84b11f39419f522fa814dcadf66ebe
Feynman43 <mohammed.znitar@praguecollege.cz>
2021-11-16 01:46:22 +0100

    json generator for park description and adding list of parks

--- src/symbolic_computation_ica1/scraper.clj
+++ src/symbolic_computation_ica1/scraper.clj
@@ -18,41 +18,22 @@
 (def vojanovy-sady "./resources/parks_html/vojanovy_sady.html")
 (def vysehrad "./resources/parks_html/vysehrad.html")
 
-;;parks as a list of strings list
-;;The order of this one shall remain the same
-(def parks-str-list (map str (keys (ns-publics 'scraper))))
-
 ;;parks of files references
 (def parks-list
-  (list
-    parks-list
-    letenske-sady
-    stromovka
-    klamovka
-    bertramka
-    text-extract
-    vysehrad
-    kinskeho-zahrada
-    petrin
-    text-extract-keys
-    riegrovy-sady
-    obora-hvezda
-    vojanovy-sady
-    text-extract-values
-    parks-list
-    kampa
-    frantiskanska-zahrada
-    key-sanitizer
-    map-generator
-    ladronka
-    sanitizer))
+  '("letenske-sady"
+     "stromovka"
+     "klamovka"
+     "bertramka"
+     "vysehrad"
+     "kinskeho-zahrada"
+     "petrin"
+     "riegrovy-sady"
+     "obora-hvezda"
+     "vojanovy-sady"
+     "kampa"
+     "frantiskanska-zahrada"
+     "ladronka"))
 
-;;example of mapping html source to allow selection selection
-(html/html-resource (java.io.File. letenske_sady))
-
-;;select the relevant resources on each page
-(html/select (html/html-resource (java.io.File. letenske_sady)) [:div.js-tabbed-content
-                                                                 :p])
 ;;Classes of the Values to extract:
 ;;i_restaurace, i_wc, i_misto, i_kolo, i_brusle, i_sport
 ;;i_hriste, i_mhd, i_gps, i_parking, i_cesty, i_provoz
@@ -104,35 +85,14 @@
                ))))
     )))
 
-
-(defn extractor-helper [lst func]
-  (map func lst))
-
 ;;example usage: (extractor-helper parks-list extracting-function)
 
-;;TODO: scraped data to JSON
-(json/write (text-extract letenske_sady))
-
-;;something that works
-;; create json from string, then we read it and pprint it
-(json/pprint (json/read-str (json/write-str (text-extract letenske_sady))))
-
-;;example of running/using write
-(json/write-str (text-extract bertramka "i_wc") java.util.Collection)
-
+;;exctarcting keys and values of from paks pages
 (defn text-extract-keys [park-name]
   (text-extract park-name true "key"))
 
 (defn text-extract-values [park-name]
   (text-extract park-name true "value"))
-
-(json/write letenske_sady java.util.Map {:key-fn text-extract-keys
-                                         :value-fn text-extract-values})
-;;todo: map from keys and values
-(zipmap (keys (text-extract-keys letenske_sady)) (vals (text-extract-keys letenske_sady)))
-
-;;todo: Lazyseq of keys
-(map keyword (text-extract-keys letenske_sady))
 
 ;;sanitizing output
 (defn sanitizer [str]
@@ -152,32 +112,30 @@
        (map
          sanitizer (text-extract-keys park-name))))
 
-(zipmap (key-sanitizer letenske_sady)
-        (vals "test"))
-
-;Todo:getting description of a park and initiating the map
-
-(json/pprint (json/read-str (json/write-str (assoc {} :description
-                                                      (first (map html/text
-                                                                  (html/select
-                                                                    (html/html-resource (java.io.File. letenske_sady))
-                                                                    [:p.perex])))))))
-
 ;; takes string
 (defn map-generator [park-name]
   (assoc {} (keyword park-name)
             (assoc {} :description
-                      (map html/text
+                      (first
+                        (map html/text
                            (html/select
                              (html/html-resource (java.io.File. (eval (read-string park-name))))
-                             [:p.perex])))))
+                             [:p.perex]))))))
 
-(defn json-generator [park-list ]
-  (loop [park-name park-list]
-    (let [map (map-generator park-name)]
-      (println "./resources/parks_json/park-description.json" "Line to be written")
-      (spit "./resources/parks_json/park-description.json" map :append true)))
-  )
+;;testing json generation
+(defn json-generator [park-list]
+  (spit "./resources/parks_json/park-description.json" (json/write-str
+                                                         (map-generator (first park-list))))
+  (loop [park-lst (rest park-list)]
+    (let [park-map (map-generator
+                     (json/write-str (first park-lst)))]
+      ;(spit "./resources/parks_json/park-description.json" park-map :append true)
+      ;(println "./resources/parks_json/park-description.json" park-map)
+      )
+    (and (not (nil? (rest park-lst))) (recur (rest park-lst)))
+    ))
 
+;generate JSON file
+(spit "./resources/parks_json/park-description.json" map :append true)
 
 
