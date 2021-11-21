4dd26c590cde48c954ea175e3b3fde5321c12483
Feynman43 <mohammed.znitar@praguecollege.cz>
2021-11-21 15:44:47 +0100

    #15 adding comments for functions, changing name space for consistency, removing non-used code blocks

--- src/symbolic_computation_ica1/scraping.clj
+++ src/symbolic_computation_ica1/scraping.clj
@@ -1,9 +1,9 @@
 ;;defining namespace
-(ns scraper
+(ns symbolic-computation-ica1.scraping
   (:require [net.cgrand.enlive-html :as html]
             [clojure.data.json :as json]))
 
-;;binding parks .html paths to variabes for clarity
+;;binding parks .html paths to variables for code clarity
 (def letenske-sady "./resources/parks_html/letenske_sady.html")
 (def bertramka "./resources/parks_html/bertramka.html")
 (def frantiskanska-zahrada "./resources/parks_html/frantiskanska_zahrada.html")
@@ -18,7 +18,8 @@
 (def vojanovy-sady "./resources/parks_html/vojanovy_sady.html")
 (def vysehrad "./resources/parks_html/vysehrad.html")
 
-;;parks of files references
+;;Binding a list of strings of the parks names to be used
+;; in map-generator function
 (def parks-list
   '("letenske-sady"
      "stromovka"
@@ -34,15 +35,16 @@
      "frantiskanska-zahrada"
      "ladronka"))
 
-;;Classes of the Values to extract:
+;;Classes of the values to extract specific information
+;;from the parks web pages:
 ;;i_restaurace, i_wc, i_misto, i_kolo, i_brusle, i_sport
 ;;i_hriste, i_mhd, i_gps, i_parking, i_cesty, i_provoz
 ;;i_doba
 
 ;; extract text from relevant html tags
 (defn text-extract
-  "function takes file/park name and a class (as clojure string)
-  returns text content of matching tags/attributes"
+  "function takes an html file/park name and a class (as clojure string)
+  and a wild-card element. it returns text content of matching tags/attributes"
   ([park-name]
    (map html/text
         (html/select
@@ -51,8 +53,8 @@
            :p]
           )))
 
-  ;if bool false, joker should be a html class from the lst above
-  ;if bool is true, joker should be either key or value
+  ;if bool false, wild-card should be a html class from the list above
+  ;if bool is true, wild-card should be either "key" or "value"
   ([park-name bool wild-card]
    (if (false? bool)
     (map html/text
@@ -73,8 +75,8 @@
              ))
 
       ;;getting the values for later JSON processing
-      ;;the n-th child 2 selects the font tag
-      ;;that comes as a second desendent of p tag
+      ;; the n-th child 2 selects the font tag
+      ;;that comes as a second descendant of p tag
       (if (= wild-card "value")
         (map html/text
              (html/select
@@ -85,17 +87,19 @@
                ))))
     )))
 
-;;example usage: (extractor-helper parks-list extracting-function)
-
-;;exctarcting keys and values of from paks pages
 (defn text-extract-keys [park-name]
+  "takes a park-name that's bound to html file of the park
+  returns keys of the elements to be extracted"
   (text-extract park-name true "key"))
 
 (defn text-extract-values [park-name]
+  "takes a park-name that's bound to html file of the park
+  and returns the value of values for the keys extracted previously"
   (text-extract park-name true "value"))
 
-;;sanitizing output
+
 (defn sanitizer [str]
+  "takes a string and formats the output to comply with JSON specifications"
   (clojure.string/replace
     (clojure.string/replace
       (clojure.string/replace
@@ -106,7 +110,6 @@
   )
 
 ;;sanitizing keywords
-
 (defn key-sanitizer [park-name]
   (map keyword
        (map
@@ -114,6 +117,9 @@
 
 ;; takes string
 (defn map-generator
+  "takes park name as a string only or a map in addition to the string
+   and, it constructs a map of the park name, and it's description
+   or ot merges the park name and its description with the map provided"
   ([park-name]
    (assoc {} (keyword park-name)
              (assoc {} :description
@@ -123,24 +129,9 @@
                                 (html/html-resource (java.io.File. (eval (read-string park-name))))
                                 [:p.perex]))))))
 
-  ;; merging
+  ;;merging
   ([park-name map]
    (merge (map-generator park-name) map)))
-
-;;testing json generation for one park
-(defn json-generator [park-list]
-  (spit "./resources/parks_json/park-description.json" (json/write-str
-                                                         (map-generator (first park-list))))
-  (loop [park-lst (rest park-list)]
-    (let [park-map (json/write-str
-                     (map-generator (first park-lst)))]
-      ;(spit "./resources/parks_json/park-description.json" park-map :append true)
-      (println "./resources/parks_json/park-description.json" park-map)
-      )
-    (and (not (nil? (rest park-lst))) (recur (rest park-lst)))
-    ))
-
-;; generating json-formatted description for all of parks
 
 (defn json-generator [parks-list]
   "transforms a map of parks descriptions to a JSON formatted file"
@@ -150,8 +141,3 @@
     (spit "./resources/parks_json/park-description.json"
           (json/write-str parks-map)
           :append false)))
-
-;generate JSON file
-(spit "./resources/parks_json/park-description.json" map :append true)
-
-
